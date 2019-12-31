8bc604750a2dd2166e66aebf61abb3fb0e9f1f88
qnedy <altmaisefequatro@gmail.com>
2019-12-31 00:58:47 -0300

    finished

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function ButtonFom(){
     return (
-        <button>Converter</button>
+        <button type="submit" name="btnConvert">Converter</button>
     )
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,33 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 
-const inputs = [
-    {
-        name: 'bin',
-        key: 'binario',
-        label: 'Binário',
-        value: 0
-    },
-    {
-        name: 'dec',
-        key: 'decimal',
-        label: 'Decimal',
-        value: 0
-    },
-];
-
 export default function Formulario(){
+    let [numBin, setNumbin] = useState(0);
+    let [numDec, setNumdec] = useState(0);
+
+    let somaDec = 0;
+
+    function handleSubmit(e){
+        e.preventDefault();
+        let isABinNumber = numBin.match("^[01]+$");
+        if(isABinNumber){
+            let cont = 0;
+            for(let i = numBin.length; i > 0; i--){
+                somaDec += (Math.pow(2, cont)) * numBin[i - 1];
+                cont++;
+            }
+            setNumdec(somaDec);
+        }
+        else{
+            alert("Insira somente números binários. Ex: 0110");
+        }
+    }
+
+    function handlePressed(){
+        if(numBin.length > 8){
+            alert("O número pode ter no máximo 8 caracteres.");
+        }
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <fieldset>
-                {
-                    inputs.map(item => (
-                        <label key={item.key}>
-                            <span>{item.label}: </span>
-                            <input type="number" name={item.name} value={item.value} onChange={e => e.taget.value}/>
-                        </label>
-                    ))
-                }
+                <label key="binario">
+                    <span>Binário: </span>
+                    <input type="number" name="bin" value={numBin} onChange={e => setNumbin(e.target.value)} onKeyPress={handlePressed}/>
+                </label>
+                <label key="decimal">
+                    <span>Decimal: </span>
+                    <input type="number" name="dec" value={numDec} onChange={e => setNumdec(e.target.value)} />
+                </label>
                 <Button />
             </fieldset>
         </form>
